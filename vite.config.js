0be0f48a38cdd45b21ac9647f17649fa0e456cc5
Masud044 <ranamasud0701@gmail.com>
2025-09-06 17:16:26 +0600

    added the issue

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+// Use process.env.API_BASE for proxy in Vite config
+// eslint-disable-next-line no-undef
+// const API_BASE = process.env.API_BASE || 'http://103.172.44.99:8989/api_bwal'
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
       "/api": {
-        target: import.meta.env.VITE_API_BASE,
+      target: "http://103.172.44.99:8989/api_bwal",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
